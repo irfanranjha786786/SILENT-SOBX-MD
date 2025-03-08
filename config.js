cece803e001b2a94920dbeae64812ba4a407e82b
irfanranjha786786 <irfanranjha786787866@gmail.com>
2025-03-09 00:47:06 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "2FomhYYC#UxoP_Uzt_3y1pR1kT78fE_IjDNzdxYZNyW648Kryd9w",
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
